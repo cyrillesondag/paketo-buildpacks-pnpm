141b296abea8c8813d6ac188867f9bdb38476729
Cyrille Sondag <cyrille.sondag@gmail.com>
2025-07-27 13:01:19 +0200

    fix: failed lint

--- run/main.go
+++ run/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/paketo-buildpacks/packit/v2/postal"
 	"github.com/paketo-buildpacks/packit/v2/sbom"
 	"github.com/paketo-buildpacks/packit/v2/scribe"
-	"github.com/paketo-buildpacks/pnpm"
 )
 
 type Generator struct{}
